986ec6df0e36e67e6813f53fb901c15d83c6e40f
88885 <V.Gucu@rendez-vous.ru>
2025-11-11 18:04:12 +0300

    Update definitions.py

--- ci/docker/app/dagster_easy/definitions.py
+++ ci/docker/app/dagster_easy/definitions.py
@@ -1,11 +1,12 @@
 import dagster as dg
 from dagster_easy.test_examples.definitions import test_examples
-
+from dagster_easy.repo_defs.healthcheck import healthcheck
 
 import warnings
 warnings.filterwarnings("ignore", category=dg.ExperimentalWarning)
 
 test_examples = test_examples
+healthcheck = healthcheck
 
 
 
