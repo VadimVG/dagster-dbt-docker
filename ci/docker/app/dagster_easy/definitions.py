908459f6507d4109a52f7c6858559ae2005d1450
Vadim <115628007+VadimVG@users.noreply.github.com>
2026-09-16 16:00:06 +0300

    rename test files

--- ci/docker/app/dagster_easy/definitions.py
+++ ci/docker/app/dagster_easy/definitions.py
@@ -1,6 +1,8 @@
 import dagster as dg
-from dagster_easy.test_examples.definitions import test_examples
+
+from dagster_easy.test_examples.repo_defs.test_examples import test_examples
 from dagster_easy.repo_defs.healthcheck import healthcheck
+
 
 import warnings
 warnings.filterwarnings("ignore", category=dg.ExperimentalWarning)
